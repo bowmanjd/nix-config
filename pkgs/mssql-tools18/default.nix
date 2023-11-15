292b77fbeb7ccc45ac51f9b5186d35ceed9d4162
Jonathan Bowman <git@bowmanjd.com>
2023-11-15 06:41:18 -0500

    attempting to fix bcp missing files

--- pkgs/mssql-tools18/default.nix
+++ pkgs/mssql-tools18/default.nix
@@ -1,23 +1,35 @@
-{ lib, stdenv, ... }:
+{ lib, stdenv, fetchurl, dpkg, patchelf, unixODBC, ... }:
 let version = "18.2.1.1-1";
 in
 stdenv.mkDerivation {
   pname = "mssql-tools18";
+  name = "mssql-tools18";
 
-  src = nixpkgs.fetchurl {
+  src = fetchurl {
     url = "https://packages.microsoft.com/debian/11/prod/pool/main/m/mssql-tools18/mssql-tools18_${version}_amd64.deb";
     sha256 = "70f219b0d7a4d4a9ff3596164a9018bf5bbc61d4313c83186010f81b3f292218";
   };
 
+  nativeBuildInputs =
+    [
+      dpkg
+      patchelf
+    ];
+
   unpackPhase = ''
       dpkg -x $src ./
     '';
+
   installPhase = ''
-    mkdir -p $out/lib
     mkdir -p $out/bin
     cp opt/mssql-tools18/bin/bcp $out/bin/bcp
+    mkdir -p $out/opt/mssql-tools18/
+    cp -r opt/mssql-tools18/share $out/opt/mssql-tools18/
     cp -r opt/mssql-tools18/share $out/
-    mkdir -p $out/bin
+  '';
+
+  postFixup = lib.optionalString stdenv.isLinux ''
+    patchelf --set-rpath ${lib.makeLibraryPath [ unixODBC stdenv.cc.cc ]} $out/bin/bcp
   '';
 
   meta = {
