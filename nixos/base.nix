1178d7de7b88f8b66d95ffae0976dd5e7904c2fd
Jonathan Bowman <jbowman@cargas.com>
2023-11-16 16:55:52 -0500

    fix library name

--- nixos/base.nix
+++ nixos/base.nix
@@ -73,7 +73,7 @@
   environment.etc."odbcinst.ini".text = ''
     [ODBC Driver 18 for SQL Server]
     Description=Microsoft ODBC Driver 18 for SQL Server
-    Driver=${lib.makeLibraryPath [ pkgs.unixODBCDrivers.msodbcsql18 ]}/libmsodbcsql-18.3.so.1.1
+    Driver=${lib.makeLibraryPath [ pkgs.unixODBCDrivers.msodbcsql18 ]}/libmsodbcsql-18.1.so.1.1
     
   '';
 # fd -t f libmsodbcsql-18 /nix/store | head -n 1
