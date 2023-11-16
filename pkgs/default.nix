638251eac0d3682cd2c4b11ab4b6a85d50e127ba
Jonathan Bowman <jbowman@cargas.com>
2023-11-16 16:53:47 -0500

    fix mssql-bcp naming

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,5 @@
 { pkgs ? import <nixpkgs> { }, ... }: rec {
   basescripts = pkgs.callPackage ./basescripts { };
   workscripts = pkgs.callPackage ./workscripts { };
-  mssql-tools18 = pkgs.callPackage ./mssql-bcp { };
+  mssql-bcp = pkgs.callPackage ./mssql-bcp { };
 }
