3f0d42367fc17deb64bfb68ef2b9b0754d0b166e
Jonathan Bowman <jbowman@cargas.com>
2023-11-19 16:53:04 -0500

    combined db and zdadbod scripts

--- pkgs/workscripts/scripts/db.sh
+++ pkgs/workscripts/scripts/db.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+ESCAPED_PASSWORD=$(python -c "import urllib.parse; print(urllib.parse.quote('$SQLCMDPASSWORD'),end='')")
+
+DBEXTRA="?trustServerCertificate&encrypt"
+DBURL="sqlserver://$SQLCMDUSER:$ESCAPED_PASSWORD@$SQLSERVER:$SQLPORT"
+export DB_UI_CargasEnergy="$DBURL/CargasEnergy$DBEXTRA"
+export DB_UI_CargasEnergyTest="$DBURL/CargasEnergyTest$DBEXTRA"
+export DB_UI_ConversionScripts="$DBURL/ConversionScripts$DBEXTRA"
+export DB_UI_LegacyData="$DBURL/LegacyData$DBEXTRA"
+
 export SQLCMDSERVER="$SQLSERVER,$SQLPORT"
 
 export CUSTOMER=$(sqlcmd -C -l 1 -W -h -1 -Q "SET NOCOUNT ON; SELECT TOP 1 Name FROM bCompany" 2> /dev/null)
