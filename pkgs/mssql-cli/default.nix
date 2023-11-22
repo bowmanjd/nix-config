95ca83e9b212b1f7753ce2c60cc5d513615c7128
Jonathan Bowman <git@bowmanjd.com>
2023-11-21 19:33:29 -0500

    trying to disable tests

--- pkgs/mssql-cli/default.nix
+++ pkgs/mssql-cli/default.nix
@@ -1,14 +1,74 @@
-{ lib, python311 }:
-with python3Packages;
+{ lib, python311, conftest, fetchFromGitHub }:
 python311.pkgs.buildPythonApplication rec {
   pname = "mssql-cli";
   version = "1.0";
 
-  #propagatedBuildInputs = [ flask ];
+  propagatedBuildInputs = with python311.pkgs; [
+    applicationinsights
+    cli-helpers
+    click
+    configobj
+    future
+    humanize
+    prompt-toolkit
+    pygments
+    pyyaml
+    sqlparse
+    wheel
+  ];
+
+  checkInputs = [
+    conftest
+    (python311.withPackages (ps:
+      with ps; [
+      setuptools
+      coverage
+      pytest-cov
+      pytest-timeout
+      mock
+      twine
+      tox
+      jinja2
+      diff-cover
+      appdirs
+      hypothesis
+      pylint
+      regex
+      twine
+      flake8
+      tblib
+      tqdm
+      pathspec
+      pytest
+      wrapt
+      packaging
+      six
+    ]))
+  ];
 
   src = fetchFromGitHub {
-    owner = bowmanjd;
-    repo = mssql-cli;
+    owner = "bowmanjd";
+    repo = "mssql-cli";
+    rev = "HEAD";
     hash = "sha256-kUc3y9OlaQ72MsESrVd+eqm4xulFixYMKAIMeP3+NOc=";
+  };
+
+  checkPhase = ''
+    #runHook preCheck
+
+    #pytest
+
+    #runHook postCheck
+  '';
+
+  meta = {
+    description = "mssql-cli interactive command-line tool for querying SQL Server";
+    license = lib.licenses.bsd3;
+    maintainers = [ {
+      email = "git@bowmanjd.org";
+      github = "bowmanjd";
+      githubId = 86415;
+      name = "Jonathan Bowman";
+    } ];
   };
 }
