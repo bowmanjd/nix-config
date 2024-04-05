b6b165f93ff2206cb10325db39f22cebb030596d
Jonathan Bowman <git@bowmanjd.com>
2024-04-05 06:43:12 -0400

    add llama-index

--- home-manager/base/default.nix
+++ home-manager/base/default.nix
@@ -210,19 +210,21 @@
         flake8-docstrings
         fonttools
         isort
-        #langchain
-        #llama-index-core
-        #llama-index-llms-openai
-        #llama-index-program-openai
-        #llama-index-llms-openai
-        #llama-index-readers-file
-        #llama-index-readers-json
-        #llama-index-readers-database
-        #llama-index-readers-llama-parse
-        #llama-index-vector-stores-qdrant
-        #llama-index-vector-stores-chroma
+        langchain
+        llama-index-core
+        llama-index-program-openai
+        llama-index-llms-openai
+        llama-index-llms-openai-like
+        llama-index-llms-ollama
+        llama-index-embeddings-openai
+        llama-index-embeddings-ollama
+        llama-index-embeddings-huggingface
+        llama-index-readers-file
+        llama-index-readers-json
+        llama-index-readers-database
+        llama-index-vector-stores-qdrant
+        llama-index-vector-stores-chroma
         lxml
-        #nltk
         pep8-naming
         pexpect
         pip
