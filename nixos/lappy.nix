4b30942dcd9b5d89089240015e3b0c15e969a783
Jonathan Bowman <git@bowmanjd.com>
2024-05-25 08:50:28 -0400

    disable NetworkManager-wait-online.service

--- nixos/lappy.nix
+++ nixos/lappy.nix
@@ -20,7 +20,8 @@
 
   nix.settings.trusted-users = [ "root" ];
 
-  systemd.network.wait-online.enable = false;
+  #systemd.network.wait-online.enable = false;
+  systemd.services.NetworkManager-wait-online.enable = false;
 
   boot.blacklistedKernelModules = ["psmouse"];
   boot.loader.systemd-boot.enable = true;
