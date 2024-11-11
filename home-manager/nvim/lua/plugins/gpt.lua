b8ab531040165cef103b11d64b98bf010dba242f
Jonathan Bowman <jbowman@cargas.com>
2024-11-11 08:46:25 -0500

    disable gpt for now

--- home-manager/nvim/lua/plugins/gpt.lua
+++ home-manager/nvim/lua/plugins/gpt.lua
@@ -3,7 +3,7 @@ return {
 		"jackMort/ChatGPT.nvim",
 		event = "VeryLazy",
 		opts = {
-			api_key_cmd = "agegent " .. home .. "/.local/share/secrets/openai.enc.txt",
+			--api_key_cmd = "agegent " .. home .. "/.local/share/secrets/openai.enc.txt",
 		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
