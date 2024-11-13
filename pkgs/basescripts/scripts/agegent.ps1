80c7cb6d9666a0aaae27544cc6a48f9afb657244
Jonathan Bowman <git@bowmanjd.com>
2024-11-13 07:25:28 -0500

    working powershell agegent

--- pkgs/basescripts/scripts/agegent.ps1
+++ pkgs/basescripts/scripts/agegent.ps1
@@ -1,35 +1,31 @@
 #!/usr/bin/env pwsh
+param(
+  [Parameter(Mandatory = $False)]
+  [System.IO.FileInfo] $filename,
+  [Parameter(Mandatory = $False, ValueFromPipeline = $True)]
+  [String] $ciphertext
+)
 
 $homedir = Resolve-Path ~
 $cipher = "$homedir/.ssh/secrets/age.key"
 $secrets = Join-Path ([Environment]::GetFolderPath('LocalApplicationData'))  ".bubba"
 $secret = Join-Path $secrets "age.key"
 
-if (-not (Test-Path $secrets)) {
-  New-Item -ItemType Directory -Path $secrets
+if (-not (Test-Path $secrets))
+{
+  New-Item -ItemType Directory -Path $secrets | Out-Null
 }
 
-if (-not (Test-Path $secret)) {
-  New-Item -ItemType File -Path $secret
+if (-not (Test-Path $secret))
+{
+  New-Item -ItemType File -Path $secret | Out-Null
   rage -d $cipher -o $secret
 }
 
-if [ -t 0 ]; then 
-  if [ $# -ne 0 ]; then 
-    ENCRYPTEDFILE="$1"
-  fi
-else
-  TMPFILE="$(mktemp)"
-  while IFS='' read -r line; do
-    printf "%s\n" "$line"
-  done > "$TMPFILE"
-  ENCRYPTEDFILE="$TMPFILE"
-fi
-
-if [ -f "$ENCRYPTEDFILE" ]; then
-  rage -d -i "$secret" < "$ENCRYPTEDFILE"
-fi
-
-if [ -f "$TMPFILE" ]; then
-  rm "$TMPFILE"
-fi
+if ($filename)
+{
+  Get-Content -Raw $filename | rage -d -i $secret
+} elseif ($ciphertext)
+{
+  Write-Output $ciphertext | rage -d -i $secret
+}
