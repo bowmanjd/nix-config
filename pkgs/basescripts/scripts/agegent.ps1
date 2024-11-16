895c5a7ef9cad37103c06d442903e102eeeda07e
Jonathan Bowman <git@bowmanjd.com>
2024-11-16 06:54:40 -0500

    agegent function

--- pkgs/basescripts/scripts/agegent.ps1
+++ pkgs/basescripts/scripts/agegent.ps1
@@ -1,31 +1,35 @@
 #!/usr/bin/env pwsh
-param(
-  [Parameter(Mandatory = $False)]
-  [System.IO.FileInfo] $filename,
-  [Parameter(Mandatory = $False, ValueFromPipeline = $True)]
-  [String] $ciphertext
-)
 
-$homedir = Resolve-Path ~
-$cipher = "$homedir/.ssh/secrets/age.key"
-$secrets = Join-Path ([Environment]::GetFolderPath('LocalApplicationData'))  ".bubba"
-$secret = Join-Path $secrets "age.key"
+function agegent
+{
+  param(
+    [Parameter(Mandatory = $False)]
+    [System.IO.FileInfo] $filename,
+    [Parameter(Mandatory = $False, ValueFromPipeline = $True)]
+    [String] $ciphertext
+  )
 
-if (-not (Test-Path $secrets))
-{
-  New-Item -ItemType Directory -Path $secrets | Out-Null
-}
+  $homedir = Resolve-Path ~
+  $cipher = "$homedir/.ssh/secrets/age.key"
+  $secrets = Join-Path ([Environment]::GetFolderPath('LocalApplicationData'))  ".bubba"
+  $secret = Join-Path $secrets "age.key"
 
-if (-not (Test-Path $secret))
-{
-  New-Item -ItemType File -Path $secret | Out-Null
-  rage -d $cipher -o $secret
-}
+  if (-not (Test-Path $secrets))
+  {
+    New-Item -ItemType Directory -Path $secrets | Out-Null
+  }
 
-if ($filename)
-{
-  Get-Content -Raw $filename | rage -d -i $secret
-} elseif ($ciphertext)
-{
-  Write-Output $ciphertext | rage -d -i $secret
+  if (-not (Test-Path $secret))
+  {
+    New-Item -ItemType File -Path $secret | Out-Null
+    rage -d $cipher -o $secret
+  }
+
+  if ($filename)
+  {
+    Get-Content -Raw $filename | rage -d -i $secret
+  } elseif ($ciphertext)
+  {
+    Write-Output $ciphertext | rage -d -i $secret
+  }
 }
