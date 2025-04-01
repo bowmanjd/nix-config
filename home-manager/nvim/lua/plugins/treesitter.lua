1a9e9c6c0da10c60625c1eaafdbbc47cd750db99
Jonathan Bowman <jbowman@cargas.com>
2025-04-01 15:14:49 -0400

    fix: TS launching

--- home-manager/nvim/lua/plugins/treesitter.lua
+++ home-manager/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,20 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		dir = treesitterPath,
 		opts = {
 			auto_install = false,
 			ensure_installed = {},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
+			-- highlight = {
+			-- 	enable = true,
+			-- 	additional_vim_regex_highlighting = false,
+			-- },
+			-- indent = {
+			-- 	enable = true,
+			-- },
+			-- incremental_selection = {
+			-- 	enable = true,
+			-- },
 		},
 	},
 }
--- home-manager/nvim/nvim.lua
+++ home-manager/nvim/nvim.lua
@@ -24,5 +24,18 @@ require("lazy").setup({
 
 vim.opt.rtp:append(treesitterPath)
 vim.opt.rtp:append(treesitterGrammars)
+
 package.cpath = package.cpath .. jsregexpPath
 package.cpath = package.cpath .. tiktokenCorePath
+
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	incremental_selection = {
+		enable = true,
+	},
+})
