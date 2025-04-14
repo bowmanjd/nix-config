8e1e1956941b79cdcf8cb9f2f9216b5466bbb3d6
Jonathan Bowman <jbowman@cargas.com>
2025-04-14 06:14:46 -0400

    remove residual nixos-wsl

--- nixos/old-nixos-wsl/tests/username-change/username-change.nix
+++
@@ -1,9 +0,0 @@
-{
-  imports = [
-    <nixos-wsl/modules>
-  ];
-
-  wsl.enable = true;
-  wsl.nativeSystemd = false;
-  wsl.defaultUser = "different-name";
-}
--- nixos/old-nixos-wsl/utils/default.nix
+++
@@ -1,8 +0,0 @@
-{ rustPlatform }:
-rustPlatform.buildRustPackage {
-  pname = "nixos-wsl-utils";
-  version = "1.0.0";
-
-  src = ./.;
-  cargoLock.lockFile = ./Cargo.lock;
-}
