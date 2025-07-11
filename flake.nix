b5a6825dcda53352d6366f18415b1c7ea9fb21a2
Jonathan Bowman <jbowman@cargas.com>
2025-07-11 11:13:04 -0400

    flake update and additional patches for sqruff

--- flake.nix
+++ flake.nix
@@ -9,19 +9,25 @@
     # Home manager
     home-manager.url = "github:nix-community/home-manager/";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
+    rust-overlay,
     ...
   } @ inputs: let
     inherit (self) outputs;
     inherit (nixpkgs) lib;
     systems = ["x86_64-linux"];
     forAllSystems = lib.genAttrs systems;
-    
+
     # Add stable nixpkgs as overlay
     overlay-stable = final: prev: {
       stable = import inputs.nixpkgs-stable {
@@ -37,30 +43,33 @@
       nixosConfigFile,
       homeConfigFile,
       extraModules ? [],
-    }: nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs outputs;};
-      modules = [
-        {
-          nixpkgs.overlays = [overlay-stable];
-          networking.hostName = hostname;
-        }
-        nixosConfigFile
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.${username} = import homeConfigFile;
-            extraSpecialArgs = {inherit inputs outputs;};
-          };
-        }
-      ] ++ extraModules;
-    };
+    }:
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules =
+          [
+            {
+              nixpkgs.overlays = [overlay-stable rust-overlay.overlays.default];
+              networking.hostName = hostname;
+            }
+            nixosConfigFile
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.${username} = import homeConfigFile;
+                extraSpecialArgs = {inherit inputs outputs;};
+              };
+            }
+          ]
+          ++ extraModules;
+      };
   in {
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
     packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
     overlays = import ./overlays {inherit inputs;};
-    
+
     # NixOS configuration entrypoint
     nixosConfigurations = {
       # Available through 'sudo nixos-rebuild switch --flake .#lappy386'
@@ -70,7 +79,7 @@
         nixosConfigFile = ./nixos/lappy.nix;
         homeConfigFile = ./home-manager/home.nix;
       };
-      
+
       # Available through 'sudo nixos-rebuild switch --flake .#work'
       work = mkNixosSystem {
         hostname = "jbowman-cargas";
