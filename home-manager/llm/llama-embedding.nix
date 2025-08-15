d311fe959f66367777ca79ea655c63e286874901
Jonathan Bowman <git@bowmanjd.com>
2025-08-15 06:47:40 -0400

    lighten embedding model

--- home-manager/llm/llama-embedding.nix
+++ home-manager/llm/llama-embedding.nix
@@ -8,8 +8,7 @@
     };
 
     Service = {
-      ExecStart = "${pkgs.llama-cpp}/bin/llama-server -hf Mungert/Qwen3-Embedding-0.6B-GGUF:Q6_K_M --embedding --port 3383 --ctx-size 512";
-      # ExecStart = "${pkgs.llama-cpp}/bin/llama-server -hf Qwen/Qwen3-Embedding-0.6B-GGUF:Q8_0 --embedding --port 3383 --ctx-size 512";
+      ExecStart = "${pkgs.llama-cpp}/bin/llama-server -hf Qwen/Qwen3-Embedding-0.6B-GGUF:Q8_0 --embedding --port 3383 --ctx-size 512";
       Restart = "on-failure";
       RestartSec = "10s"; # what does this do?
 
